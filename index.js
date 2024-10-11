8e98048828c3233f6d0542a7cd21f3208b7c628d
melina-escalante <meliescalantee@gmail.com>
2024-10-11 16:56:50 -0300

    Created filter for sneaker and getuserbyname and get brandbyname

--- index.js
+++ index.js
@@ -4,28 +4,7 @@ const db = require('./config/database.js')
 const fs = require('fs')
 require('dotenv').config()
 const port = process.env.PORT
-//LLenamos la db
-const importData = async () => {
-    try {
-        const vuelosData = JSON.parse(fs.readFileSync('./data/arcana.vuelos.json, utf - 8'));
-        console.log('"Vuelos:', vuelosData);
-        const usersData = JSON.parse(fs.readFileSync('./data/arcana.users.json, utf - 8'));
-        console.log(usersData);
-        const reservasuata = JSON.parse(fs.readFilesync('./data/arcana.reservas.json', 'utf - 8'));
-        console.log(reservasData);
 
-        await Vuelos.create(vuelosData)
-        console.log("Vuelos importados exitosamente");
-        await Users.deleteMany();
-        await Users.create(usersData)
-        console.log('Usuarios importados exitosamente')
-        await Reservas.deleteMany()
-        await Reservas.create(reservasData);
-        console.log('Reservas importadas exitosamente')
-    } catch (error) {
-        console.error('Error al importar datos: ', error);
-    }
-};
 //Middleware de express
 const app = express()
 app.use(express.json())
